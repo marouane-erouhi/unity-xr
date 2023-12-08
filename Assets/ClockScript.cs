fbf040055d56f70487734ce5598c46b8e68aff7e
Marouane <13183518+marouane-erouhi@users.noreply.github.com>
2023-12-08 06:04:41 -0500

    Setup device simulator, added a room with lighting to build the rest of the project in; added the basic VR rig

---
+++ Assets/ClockScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockScript : MonoBehaviour
+{
+    public GameObject hours_hand;
+    public GameObject minutes_hand;
+    public GameObject secconds_hand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
